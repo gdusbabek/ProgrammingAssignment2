e90c0b9e208e8443d004b250d4e65719e98e31b0
gdusbabek <gdusbabek@gmail.com>
2014-05-19 09:18:42 -0500

    programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I did not follow the pattern establisedh by the makeVector/cachemean
+## examples. This implementation simplifies by pushing the cache check
+## down into the 'special' matrix object.
+##
+## Test it ithis way:
+##
+## x <- matrix(rnorm(100), 10, 10)  # creates the original matrix
+## xinv <- solve(x) # actual inverse of x.
+## xspecial <- makeCacheMatrix(x)  # creates the special version of x
+## xspecial$isCalculated() # should return FALSE because we haven't calculated the inverse
+## xspecialInv <- xspecial$getInverse() # calculates the inverse.
+## xspecial$isCalculated() # should return TRUE because we've calculated the inverse.
+## identical(xinv, xspecialInv) # should return TRUE because they are both the inverse of x
 
-## Write a short comment describing this function
-
+## returns a collection of functions 
 makeCacheMatrix <- function(x = matrix()) {
-
+  cachedInverse <- NULL
+  
+  # handy way to test of the inverse is already calculated.
+  # used for testing.
+  isCalculated <- function() {
+    !is.null(cachedInverse)
+  }
+  
+  ## do the 'solve' part here. if the cache is NULL compute the cache,
+  ## then return the case.
+  getInverse <- function() {
+    if (is.null(cachedInverse)) {
+      cachedInverse <<- solve(x)
+    }
+    return (cachedInverse)
+  }
+  
+  # return getInverse and isCalculated
+  list(getInverse = getInverse,
+       isCalculated = isCalculated)
 }
 
 
-## Write a short comment describing this function
-
+## fetch the inverse from the object.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  x$getInverse()
 }
+
